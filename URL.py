f4964e89ccf33d24b680ab4a67cb9706f9f3641d
bindaldhara <84957152+bindaldhara@users.noreply.github.com>
2021-09-28 14:30:50 +0530

    Update URL.py

--- URL.py
+++ URL.py
@@ -1,6 +1,6 @@
 import pyshorteners #library in python for shortening URL
-def shorten_url(url):
-    return pyshorteners.Shortener().tinyurl.short(url)
+def shorten(url):
+    return pyshorteners.Shortener().tinyurl.short(url) ## shortening the URL
 
 url = input("Enter URL: ") #takes input(URL) from the user
-print("Shortened URL : ", shorten_url(url)) 
+print("Shortened URL : ", shorten(url)) #returns output
